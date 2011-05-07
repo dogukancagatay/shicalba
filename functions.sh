307eb6ce77ca141ce6db48a24a6dc48269d085d6
Dogukan Cagatay <dcagatay@gmail.com>
2011-05-07 13:54:36 +0000

    added some functions

--- functions.sh
+++ functions.sh
@@ -29,22 +29,46 @@ function findTimeZoneHourSet() {
 
 #findTimeZoneHourSet
 
-## create icalendar datetime for now
-function createIcalendarDatetimeNow() {
+## create icalendar datetime $1:day $2:month $3: year $4:hour $5:minute
+## if no argument exists output will be the current datetime
+## if only 1 argument exists it creates date for DTSTAMP
+## if 3 arguments exist hour and minute values are 0
+function createIcalendarDatetime() {
+	if (( $#==0 )); then
+		DATEFIELD=$(date +%Y%m%d)
+		HOURFIELD=$(date +%H%M%S)
+	elif (( $#==1 )); then
+		DATEFIELD=$(date +%Y%m%d)
+		HOURFIELD="000000"
+	elif (( $#==3 )); then
+		DATEFIELD="$3$2$1"
+		HOURFIELD="000000"
+	else
+		DATEFIELD="$3$2$1"
+		HOURFIELD="$4$5""00"
+	fi
 	
-	DATEFIELD=$(date +%Y%m%d)
-	HOURFIELD=$(date +%H%M%S)
 	ICALDATETIME="$DATEFIELD""T""$HOURFIELD""Z"
 	echo $ICALDATETIME
 }
 
+#createIcalendarDatetime
+#createIcalendarDatetime 03 04 2011
+#createIcalendarDatetime 03 04 2011 13 22
+
 ## create icalendar UID
 function createIcalendarUID() {
-	export -f createIcalendarDatetimeNow
-	NOW=$(createIcalendarDatetimeNow)
-	echo "$NOW""-""$(date | shasum | head -c 7)""-@""$(echo $LOGNAME | shasum | head -c 5)"
+	#export -f createIcalendarDatetime
+	NOW=$(createIcalendarDatetime)
+	echo "$NOW""-""$(date | shasum | head -c 7)""-""$(echo $LOGNAME | shasum | head -c 2)""@""$(echo $HOSTNAME | shasum | head -c 3)"
 }
 
 createIcalendarUID
 
+## create icalendar DTSTAMP
+function createIcalendarDTSTAMP() {
+	DATETIME=$(createIcalendarDatetime DTSTAMP)
+	echo $DATETIME
+}
 
+createIcalendarDTSTAMP
